12a66e103e2f4e3ed6096158f73d711496db5bcb
Tom Yates <tomy8s@hotmail.com>
2016-02-26 11:41:33 +0500

    more messing around with SQL!

---
+++ sign_up/settings.php
@@ -0,0 +1,8 @@
+<?php
+//By T. Yates
+defined('DB_HOST') or define ('DB_HOST','localhost');
+defined('DB_USER') or define ('DB_USER','root');
+defined('DB_NAME') or define ('DB_NAME','`site`.`user_id`');
+defined('DB_PASSWORD') or define ('DB_PASSWORD','');
+//By T. Yates
+?>
--- sign_up/sign_up.php
+++ sign_up/sign_up.php
@@ -1,26 +1,22 @@
 <?php
-$fna = $_POST['fname'];
-$sna = $_POST['sname'];
-$pen = $_POST['penis'] == 'yes';
-$nna = $_POST['nname'];
-$una = $_POST['uname'];
-$pas = $_POST['pword'];
-$ema = $_POST['email'];
-$dob = $_POST['dob'];
-$loc = $_POST['loc'];
-$tim = intval($_POST['tz']);
+//By T. Yates
+require_once('settings.php');
+echo $fna = $_POST['fname'];
+echo $sna = $_POST['sname'];
+echo $pen = $_POST['penis'] == 'yes'? 1 : 0;
+echo $nna = $_POST['nname'];
+echo $una = $_POST['uname'];
+echo $pas = $_POST['pword'];
+echo $ema = $_POST['email'];
+echo $dob = $_POST['dob'];
+echo $loc = $_POST['loc'];
+echo $tim = intval($_POST['tz']);
 
-print_r($_POST);
+//print_r($_POST);
 
-mysqli_connect();
-mysql_query('INSERT INTO `site`.`user_info`(`First_name`) VALUES (`$fna`);');
-mysql_query('INSERT INTO `site`.`user_info`(`Surname`) VALUES (`$sna`);');
-mysql_query('INSERT INTO `site`.`user_info`(`Penis`) VALUES (`$pen`);');
-mysql_query('INSERT INTO `site`.`user_info`(`Nickname`) VALUES (`$nna`);');
-mysql_query('INSERT INTO `site`.`user_info`(`Username`) VALUES (`$una`);');
-mysql_query('INSERT INTO `site`.`user_info`(`Pword`) VALUES (`$pas`);');
-mysql_query('INSERT INTO `site`.`user_info`(`Email`) VALUES (`$ema`);');
-mysql_query('INSERT INTO `site`.`user_info`(`Date_of_birth`) VALUES (`$dob`);');
-mysql_query('INSERT INTO `site`.`user_info`(`Location`) VALUES (`$loc`);');
-mysql_query('INSERT INTO `site`.`user_info`(`Time_zone`) VALUES (`$tim`);');
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD);
+print '<br>'.($connection) ? 'connected' : mysqli_connect_error() ;
+//mysql_query("INSERT INTO `site`.`user_info` VALUES (NULL,'$fna','$sna','$pen','$una','$pas','$ema','$dob','$loc','$tim');");
+echo mysql_query("SELECT * FROM `site`.`user_info`");
+//By T. Yates
 ?>
