56644dd3cce6658e826b443fe7d248c302a518c5
Tom Yates <tomy8s@hotmail.com>
2016-03-02 12:50:37 +0500

    Now it works right, next job: select a word from anarray!

--- projects/4_letters/four_letters.js
+++ projects/4_letters/four_letters.js
@@ -1,52 +1,47 @@
 //By T.Yates
 
-function click() {
-    if (document.getElementById('guess').length != 4) {
-        alert("Please enter a 4-letter word")
-    } else {
-        play();
-    }
-}
-
 function play() {
-    var word = "fish";
-    var guess = document.getElementById('guess').value;
-    var rw = 0;
-    var rr = 0;
-    
-    for (c = 0; c < 4; c++) {
-        if (word[c] === guess[c]) {
-            rr++;
-        }else for (d = 0; d < 4; d++) {
-            if (word[c] === guess[d]) {
-                rw++;
-                continue;
+    if (document.getElementById('guess').value.length != 4) {
+        alert("Please enter a 4-letter word");
+    } else {
+        var word = "fish";
+        var guess = document.getElementById('guess').value;
+        var rw = 0;
+        var rr = 0;
+        
+        for (c = 0; c < 4; c++) {
+            if (word[c] === guess[c]) {
+                rr++;
+            }else for (d = 0; d < 4; d++) {
+                if (word[c] === guess[d]) {
+                    rw++;
+                    break;
+                }
             }
         }
-    }
-    
-    var row = document.createElement('TR');
-    var rwcell = document.createElement('TD');
-    var rwval = document.createTextNode(rw);
-    var rrcell = document.createElement('TD');
-    var rrval = document.createTextNode(rr);
-    var gucell = document.createElement('TD');
-    var guval = document.createTextNode(guess);
-    
-    rwcell.appendChild(rwval);
-    rrcell.appendChild(rrval);
-    gucell.appendChild(guval);
-    
-    row.appendChild(rwcell);
-    row.appendChild(rrcell);
-    row.appendChild(gucell);
         
-    document.getElementById("list").appendChild(row);
-    document.getElementById('guess').value = "";
-    
-    if (rr === 4) {
-        alert("Congratulations! The correct answer is "+word+"!")
+        var row = document.createElement('TR');
+        var rwcell = document.createElement('TD');
+        var rwval = document.createTextNode(rw);
+        var rrcell = document.createElement('TD');
+        var rrval = document.createTextNode(rr);
+        var gucell = document.createElement('TD');
+        var guval = document.createTextNode(guess);
+        
+        rwcell.appendChild(rwval);
+        rrcell.appendChild(rrval);
+        gucell.appendChild(guval);
+        
+        row.appendChild(rwcell);
+        row.appendChild(rrcell);
+        row.appendChild(gucell);
+            
+        document.getElementById("list").appendChild(row);
+        document.getElementById('guess').value = "";
+        
+        if (rr === 4) {
+            alert("Congratulations! The correct answer is "+word+"!")
+        }
     }
-
 }
 //By T.Yates
